5e7da69c4a2585c8573402144a35ea6315560b55
Najeeb Arif <NajeebArif786@outlook.com>
2022-05-05 23:34:33 +0530

    Add initial commit

---
+++ src/main/java/narif/poc/findmydoc/FindMyDocApplication.java
@@ -0,0 +1,13 @@
+package narif.poc.findmydoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindMyDocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/narif/poc/findmydoc/FindMyDocApplicationTests.java
@@ -0,0 +1,13 @@
+package narif.poc.findmydoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
