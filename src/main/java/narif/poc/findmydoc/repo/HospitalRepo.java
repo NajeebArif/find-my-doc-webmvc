676d2e662d896f107ef3f9fe087249f5bab60d7a
Najeeb Arif <NajeebArif786@outlook.com>
2022-05-06 13:28:24 +0530

    Add method to find by name

--- src/main/java/narif/poc/findmydoc/repo/HospitalRepo.java
+++ src/main/java/narif/poc/findmydoc/repo/HospitalRepo.java
@@ -3,5 +3,8 @@ package narif.poc.findmydoc.repo;
 import narif.poc.findmydoc.model.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HospitalRepo extends JpaRepository<Hospital, Long> {
+    Optional<Hospital> findByName(String name);
 }
--- src/test/java/narif/poc/findmydoc/repo/HospitalRepoTest.java
+++ src/test/java/narif/poc/findmydoc/repo/HospitalRepoTest.java
@@ -1,29 +1,43 @@
 package narif.poc.findmydoc.repo;
 
 import narif.poc.findmydoc.model.entity.Hospital;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 class HospitalRepoTest {
 
+    public static final String AHMAD_HOSPITALS = "Ahmad Hospitals";
     @Autowired
     private HospitalRepo hospitalRepo;
 
-    @Test
-    void testHospitalRepo(){
+    @BeforeEach
+    public void init(){
         Hospital hospital = createHospital();
         hospitalRepo.save(hospital);
+    }
+
+    @Test
+    void testHospitalRepo(){
         Hospital hospital1 = hospitalRepo.findAll().get(0);
-        assertThat(hospital1.getName()).isEqualTo("Ahmad Hospitals");
+        assertThat(hospital1.getName()).isEqualTo(AHMAD_HOSPITALS);
+    }
+
+    @Test
+    void testFindByName(){
+        Optional<Hospital> byName = hospitalRepo.findByName(AHMAD_HOSPITALS);
+        assertThat(byName).isNotEmpty();
     }
 
     static Hospital createHospital() {
         Hospital hospital = new Hospital();
-        hospital.setName("Ahmad Hospitals");
+        hospital.setName(AHMAD_HOSPITALS);
         hospital.setAddress("Patna, Bihar");
         hospital.setPhone("22222222");
         hospital.setEmail("helpdesk@ahmadhospitals.com");
