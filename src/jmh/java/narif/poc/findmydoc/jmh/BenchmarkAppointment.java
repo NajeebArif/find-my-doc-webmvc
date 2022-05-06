a8c371260421de85d85ac416f8dca4afd3b99b26
Najeeb Arif <NajeebArif786@outlook.com>
2022-05-07 01:46:00 +0530

    Add more heavy benchmark method

--- src/jmh/java/narif/poc/findmydoc/jmh/BenchmarkAppointment.java
+++ src/jmh/java/narif/poc/findmydoc/jmh/BenchmarkAppointment.java
@@ -10,7 +10,13 @@ import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.OptionalInt;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class BenchmarkAppointment {
 
@@ -21,12 +27,29 @@ public class BenchmarkAppointment {
         public AppointmentDto appointmentDto;
         public RestTemplate restTemplate;
         public HttpEntity<AppointmentDto> entity;
+        public List<CompletableFuture<ResponseEntity<String>>> allResults = new ArrayList<>();
 
         @Setup(Level.Invocation)
         public void init(){
             appointmentDto = createAppointment("Najeeb", "Arif","AIIMS, Patna");
             entity = new HttpEntity<>(appointmentDto);
             restTemplate = new RestTemplate();
+        }
+
+        public void submitTask(CompletableFuture<ResponseEntity<String>> task){
+            if(Objects.isNull(allResults))
+                allResults = new ArrayList<>();
+            allResults.add(task);
+        }
+
+        @TearDown(Level.Trial)
+        public void tearDown(){
+            OptionalInt any = allResults.stream()
+                    .map(CompletableFuture::join)
+                    .mapToInt(ResponseEntity::getStatusCodeValue)
+                    .filter(value -> value == 200)
+                    .findAny();
+            any.ifPresent(value -> {throw new RuntimeException();});
         }
 
         private AppointmentDto createAppointment(String userName, String doctorName, String hospitalName) {
@@ -40,16 +63,35 @@ public class BenchmarkAppointment {
         }
     }
 
+//    @Benchmark
+//    @Fork(value = 1, warmups = 1)
+//    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+//    @BenchmarkMode(Mode.AverageTime)
+//    public void bookAppointment(AppointmentState appointmentState, Blackhole blackhole){
+//        HttpEntity<AppointmentDto> entity = appointmentState.entity;
+//        RestTemplate restTemplate = appointmentState.restTemplate;
+//        ResponseEntity<String> appointmentResponse = restTemplate.postForEntity(APPOINTMENTS, entity, String.class);
+//        int statusCodeValue = appointmentResponse.getStatusCodeValue();
+//        blackhole.consume(statusCodeValue);
+//    }
+
     @Benchmark
-    @Fork(value = 1, warmups = 1)
+    @Fork(value = 2, warmups = 5)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
     @BenchmarkMode(Mode.AverageTime)
-    public void init(AppointmentState appointmentState, Blackhole blackhole){
+    public void bookAppointmentParallel(AppointmentState appointmentState, Blackhole blackhole){
         HttpEntity<AppointmentDto> entity = appointmentState.entity;
         RestTemplate restTemplate = appointmentState.restTemplate;
-        ResponseEntity<String> appointmentResponse = restTemplate.postForEntity(APPOINTMENTS, entity, String.class);
-        int statusCodeValue = appointmentResponse.getStatusCodeValue();
-        blackhole.consume(statusCodeValue);
+        CompletableFuture<ResponseEntity<String>> responseEntityCompletableFuture = CompletableFuture.supplyAsync(() -> restTemplate.postForEntity(APPOINTMENTS, entity, String.class));
+        appointmentState.submitTask(responseEntityCompletableFuture);
     }
+
+//    @Benchmark
+//    @Fork(value = 2, warmups = 5)
+//    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+//    @BenchmarkMode(Mode.AverageTime)
+//    public void bookAppointmentCollectResponse(AppointmentState appointmentState, Blackhole blackhole){
+//        appointmentState.allResults.stream().map(CompletableFuture::join).forEach(blackhole::consume);
+//    }
 
 }
